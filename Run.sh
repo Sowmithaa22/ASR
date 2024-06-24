42ba1a4a396c19dd5c5a9a905bf0dc9c4c0352d3
Sowmithaa Sri S <137026975+Sowmithaa22@users.noreply.github.com>
2024-06-24 14:37:06 +0530

    Create Run.sh

---
+++ Run.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+
+kmeans_feature="vasista_hubert/21"  # use model_type/layer_index
+nclusters=2000
+
+src_lang=$(echo "${kmeans_feature}_km${nclusters}" | tr "/" "_")
+tgt_lang=ta
+
+train_set="train"
+train_dev="valid"
+test_sets="valid"
+
+asr_config=conf/train_discrete_asr_e_branchformer1_1gpu.yaml
+inference_config=conf/decode_ctc0.3.yaml
+
+src_nbpe=6000   # I use src_nbpe=6000 for 2000-cluster kmeans.
+tgt_nbpe=5000   # if token_joint is True, then only tgt_nbpe is used
+
+# ts: true sequence
+# rm: deduplicated sequence which removes duplicated tokens
+src_case="rm"
+tgt_case="ts"
+
+./asr2.sh \
+    --kmeans_opts "--batch_bins 4800000 --nj 4" \
+    --kmeans_feature "${kmeans_feature}" \
+    --nclusters "${nclusters}" \
+    --ngpu 1 \
+    --src_lang ${src_lang} \
+    --tgt_lang ${tgt_lang} \
+    --src_token_type "bpe" \
+    --src_nbpe $src_nbpe \
+    --tgt_token_type "bpe" \
+    --tgt_nbpe $tgt_nbpe \
+    --src_case ${src_case} \
+    --tgt_case ${tgt_case} \
+    --use_lm false \
+    --asr_config "${asr_config}" \
+    --inference_config "${inference_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${train_dev}" \
+    --test_sets "${test_sets}" \
+    --src_bpe_train_text "dump/extracted/vasista_hubert/layer21/org/${train_set}/text.${src_case}.${src_lang}" \
+    --tgt_bpe_train_text "dump/extracted/vasista_hubert/layer21/org/${train_set}/text.${tgt_case}.${tgt_lang}" \
+    --lm_train_text "data/${train_set}/text.${tgt_case}.${tgt_lang}" "$@" \
+    --stage 5 \
+    --stop_stage 5
